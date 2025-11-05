fb133cf5d5621c0263b9833c28cd67bce0de952a
leejunming35622792 <jlee0465@student.monash.edu>
2025-11-05 14:07:32 +0800

    Add Tab Gallery in job.html

--- js/script-job.js
+++ js/script-job.js
@@ -26,3 +26,17 @@ window.addEventListener('scroll', () => {
 document.getElementById('downloadBtn').addEventListener('click', () => {
   window.open('files/Resume.pdf', '_blank');
 });
+
+// Picture slide
+function myFunction(imgs) {
+  // Get the expanded image
+  var expandImg = document.getElementById("expandedImg");
+  // Get the image text
+  var imgText = document.getElementById("imgtext");
+  // Use the same src in the expanded image as the image being clicked on from the grid
+  expandImg.src = imgs.src;
+  // Use the value of the alt attribute of the clickable image as text inside the expanded image
+  imgText.innerHTML = imgs.alt;
+  // Show the container element (hidden with CSS)
+  expandImg.parentElement.style.display = "block";
+}
